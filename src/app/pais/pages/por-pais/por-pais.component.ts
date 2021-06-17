e24c01d8db1a3f60d7e0ae91431d379a3247f678
suriel_asael <kato_1350@hotmail.com>
2021-06-16 22:56:48 -0500

    project-paises_app add: add paisInput and paisTabla component

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -16,10 +16,11 @@ export class PorPaisComponent {
 
   constructor( private paisService: PaisService ) { }
 
-  buscar() {
+  buscar( termino: string ) {
     this.hayError = false;
+    this.termino = termino;
 
-    this.paisService.buscarPais( this.termino )
+    this.paisService.buscarPais( termino )
       .subscribe( (paises) => {
         this.paises = paises;
       }, (err) => {
