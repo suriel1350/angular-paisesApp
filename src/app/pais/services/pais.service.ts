82595165e4662eb69250e8a0b15f82037865bf1d
suriel_asael <kato_1350@hotmail.com>
2021-06-21 21:21:21 -0500

    project-paises_app add: add better HTTP Requests options

--- src/app/pais/services/pais.service.ts
+++ src/app/pais/services/pais.service.ts
@@ -1,7 +1,7 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
-import { catchError } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
 
 import { Country } from '../interfaces/pais.interface';
 
@@ -12,18 +12,22 @@ export class PaisService {
 
   private apiUrl: string = 'https://restcountries.eu/rest/v2';
 
-  constructor( private http: HttpClient ) { 
-
+  get httpParams () {
+    return new HttpParams().set( 'fields', 'name;capital;alpha2Code;flag;population' );
   }
+
+  constructor( private http: HttpClient ) { }
 
   buscarPais( termino: string ): Observable<Country[]> {
     const url = `${ this.apiUrl }/name/${ termino }`;
-    return this.http.get<Country[]>( url );
+
+    return this.http.get<Country[]>( url, { params: this.httpParams } );
   }
 
   buscarCapital( termino: string ): Observable<Country[]> {
     const url = `${ this.apiUrl }/capital/${ termino }`;
-    return this.http.get<Country[]>( url );
+
+    return this.http.get<Country[]>( url, { params: this.httpParams } );
   }
 
   getPaisPorAlpha( id: string ): Observable<Country> {
@@ -32,7 +36,10 @@ export class PaisService {
   }
 
   buscarRegion( region: string ): Observable<Country[]> {
-    const url = `${ this.apiUrl }/region/${ region }`;
-    return this.http.get<Country[]>( url );
+    const url = `${ this.apiUrl }/region/${ region }?fields=`;
+    return this.http.get<Country[]>( url, { params: this.httpParams } )
+            .pipe(
+              tap( console.log )
+            )
   }
 }
