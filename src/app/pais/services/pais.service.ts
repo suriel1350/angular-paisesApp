16ecebb912620116f7f34c838db3f416183dfa01
suriel_asael <kato_1350@hotmail.com>
2021-06-21 21:08:34 -0500

    project-paises_app add: add PorRegion content and process

--- src/app/pais/services/pais.service.ts
+++ src/app/pais/services/pais.service.ts
@@ -30,4 +30,9 @@ export class PaisService {
     const url = `${ this.apiUrl }/alpha/${ id }`;
     return this.http.get<Country>( url );
   }
+
+  buscarRegion( region: string ): Observable<Country[]> {
+    const url = `${ this.apiUrl }/region/${ region }`;
+    return this.http.get<Country[]>( url );
+  }
 }
